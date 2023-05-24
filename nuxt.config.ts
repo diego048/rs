563d3d9a937a3dc5ce7cdcf554cefe8652d6261c
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-24 00:55:40 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,30 @@ export default defineNuxtConfig({
         pageTransition: { name: 'page', mode: 'out-in' },
         head: {
             charset: 'utf-16',
-            viewport: 'width=500, initial-scale=1',
+            viewport: 'width=device-width, initial-scale=1',
             title: 'RSpharma',
             meta: [
-              { name: 'description', content: 'a' }
+              { name: 'description', content: 'Somos una empresa dedicada a la comercialización y distribución de material médico, dispositivos médicos y productos farmacéuticos oncológicos de procedencia nacional y extranjera' }
             ],
+            link: [
+            {
+              href:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+              rel:"stylesheet", 
+              integrity:"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65",
+              crossorigin:"anonymous"
+            },
+            {
+              rel:"stylesheet",
+              href:"https://cdn.materialdesignicons.com/6.4.95/css/materialdesignicons.min.css"
+            },
+          ],
+          script: [
+            {
+              src:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", 
+              integrity:"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4", 
+              crossorigin:"anonymous"
+            },
+          ]
           },
       },
 })
