d4b074d4440f465fa71efde68f9e24ea726abfce
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-29 16:09:30 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,10 +17,6 @@ export default defineNuxtConfig({
               integrity:"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65",
               crossorigin:"anonymous"
             },
-            {
-              rel:"stylesheet",
-              href:"https://cdn.materialdesignicons.com/6.4.95/css/materialdesignicons.min.css"
-            },
           ],
           script: [
             {
